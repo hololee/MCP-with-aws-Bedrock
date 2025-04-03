6e4d3adb5a7987e49aec4b2a8f4905d6e6d1fe69
hololee <lccandol@gmail.com>
2025-04-03 17:54:20 +0900

    출력 정보 정리

--- src/client/main.py
+++ src/client/main.py
@@ -1,9 +1,9 @@
 import asyncio
-
-from agent import CSVAgent
 from mcp import ClientSession
 from mcp import StdioServerParameters
 from mcp.client.stdio import stdio_client
+
+from agent import CSVAgent
 
 
 async def main():
@@ -19,26 +19,25 @@ async def main():
             await session.initialize()
             tools = await session.list_tools()
             for tool in tools.tools:
-                csv_agent.tools[tool.name.replace('-', '_')] = {
+                csv_agent.tools[tool.name] = {
+                    'name': tool.name,
                     'function': session.call_tool,
                     'description': tool.description,
                     'input_schema': {'json': tool.inputSchema},
-                    'original_name': tool.name,
                 }
 
-            response = await csv_agent.invoke([{'text': "뉴욕 날씨를 알려줘."}])
+            response = await csv_agent.invoke([{'text': "뉴욕의 날씨 예보를 알려줘."}])
 
             if response['stopReason'] == 'tool_use':
                 tool_response = []
                 for content_item in response['output']['message']['content']:
                     if 'toolUse' in content_item:
-                        tool_use_id = content_item['toolUse']['toolUseId']
                         name = content_item['toolUse']['name']
+                        tool_use_id = content_item['toolUse']['toolUseId']
                         tool_input = content_item['toolUse']['input']
                         tool_func = csv_agent.tools[name]['function']
-                        original_name = csv_agent.tools[name]['original_name']
 
-                        result = await tool_func(original_name, tool_input)
+                        result = await tool_func(name, tool_input)
                         tool_response.append(
                             {
                                 'toolResult': {
@@ -49,9 +48,13 @@ async def main():
                             }
                         )
                 response = await csv_agent.invoke(tool_response)
-                print(response)
+                for content_item in response['output']['message']['content']:
+                    if 'text' in content_item:
+                        print(content_item['text'])
             else:
-                print(response)
+                for content_item in response['output']['message']['content']:
+                    if 'text' in content_item:
+                        print(content_item['text'])
 
 
 if __name__ == "__main__":
